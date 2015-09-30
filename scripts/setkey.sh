35b02dc80f71ec37889e10c6484841c37e4daf3f
Mark Stillwell <mark@stillwell.me>
2015-09-30 15:56:07 +0100

    let's try this...

--- scripts/setkey.sh
+++ scripts/setkey.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-ATTEMPTS=30
+ATTEMPTS=10
 
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
 TMPFILE=$(mktemp)
-while [ ! -f /root/.ssh/authorized_keys ] && [ ${ATTEMPTS} -gt 0 ]; do
+while [ ${ATTEMPTS} -gt 0 ]; do
   ATTEMPTS=$((${ATTEMPTS}-1))
-  curl -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key \
-    > ${TMPFILE} 2>/dev/null
+  rm -f ${TMPFILE}
+  [ -f /root/.ssh/authorized_keys ] && cat > ${TMPFILE}
+  curl -m 5 -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key \
+    >> ${TMPFILE} 2>/dev/null
   if [ $? -eq 0 ]; then
     cat ${TMPFILE} >> /root/.ssh/authorized_keys
     chmod 0600 /root/.ssh/authorized_keys
@@ -19,6 +21,7 @@ while [ ! -f /root/.ssh/authorized_keys ] && [ ${ATTEMPTS} -gt 0 ]; do
     cat /root/.ssh/authorized_keys
     echo
     echo "********************************************************"
+    break
   fi
 done
 rm -f ${TMPFILE}
